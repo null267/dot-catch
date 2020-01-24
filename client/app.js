b19a3b0baa207778eeb14452bc8c62265f864e64
null267 <eigenheiter@gmail.com>
2020-01-24 17:30:05 -0500

    Webpack ver. 1

---
+++ client/app.js
@@ -0,0 +1,16 @@
+//App.js
+import React, { Component} from "react";
+import "./style.css";
+import {hot} from "react-hot-loader";
+
+class App extends Component{
+  render(){
+    return(
+      <div className="App">
+        <h1> .catch </h1>
+      </div>
+    );
+  }
+}
+
+export default hot(module)(App);
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require("path");
+const webpack = require("webpack");
+
+module.exports = {
+  entry: "./client/index.js",
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: "babel-loader",
+        options: { presets: ["@babel/env", "@babel/preset-react"] }
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  },
+  resolve: { extensions: ["*", ".js", ".jsx"] },
+  output: {
+    path: path.resolve(__dirname, "dist/"),
+    publicPath: "/dist/",
+    filename: "bundle.js"
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "public/"),
+    publicPath: "http://localhost:8080/dist/",
+    proxy: {
+      '/api': {
+          target: 'http://localhost:3000'
+      }
+    },
+    hotOnly: true
+  },
+  plugins: [new webpack.HotModuleReplacementPlugin()]
+};
